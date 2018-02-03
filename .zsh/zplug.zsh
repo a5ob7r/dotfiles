ea43251932b6e7d320fb6eebbd7ee4b49409a290
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2018-02-03 10:26:02 +0900

    Clean zsh conf file of zplug

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -1,7 +1,3 @@
-# -------------------------------------
-# zplug
-# -------------------------------------
-
 source $ZPLUG_HOME/init.zsh
 
 # 'zplug' self update
@@ -20,18 +16,18 @@ if zplug check "zsh-users/zsh-history-substring-search"; then
     bindkey -M emacs '^N' history-substring-search-down
 fi
 
-# zplug "b4b4r07/enhancd", use:init.sh
+zplug "b4b4r07/enhancd", use:init.sh
 
 zplug mafredri/zsh-async, from:github
 zplug sindresorhus/pure, use:pure.zsh, from:github, as:theme
 
 # Install plugins if there are plugins that have not been installed
-# if ! zplug check --verbose; then
-#     printf "Install? [y/N]: "
-#     if read -q; then
-#         echo; zplug install
-#     fi
-# fi
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    fi
+fi
 
 # Then, source plugins and add commands to $PATH
 # zplug load --verbose
