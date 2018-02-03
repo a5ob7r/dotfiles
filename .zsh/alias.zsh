078537712aecca919f2b20f9eb0d202f2a3a6a57
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2018-02-04 02:40:06 +0900

    Change zsh alias
    Add mvim(MacVim) command

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -2,9 +2,7 @@ alias grep="grep --color -n -I --exclude='*.svn-*' --exclude='entries' --exclude
 alias la="ls -hlTAFG"
 
 if [ -f /Applications/MacVim.app/Contents/MacOS/Vim ]; then
-    VIM_ALIAS='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
-    alias vi=${VIM_ALIAS}
-    alias vim=${VIM_ALIAS}
+    alias mvim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
 fi
 
 if type tree > /dev/null 2>&1; then
