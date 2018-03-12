20d794eb46fded90708e594ba694603d994f5ec5
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2018-03-12 20:44:10 +0900

    Add direnv setup to zsh conf

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -48,3 +48,6 @@ export LESS_TERMCAP_so=$'\E[00;47;30m'   # Begins standout-mode.
 export LESS_TERMCAP_ue=$'\E[0m'          # Ends underline.
 export LESS_TERMCAP_us=$'\E[01;32m'      # Begins underline.
 
+# for direnv
+eval "$(direnv hook zsh)"
+
