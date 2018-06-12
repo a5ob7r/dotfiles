741a6deb2af3ea55bfefb10f37cafd7e1efa6436
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2018-06-12 11:49:39 +0900

    Fix setup scripts

--- setup/install_formula.sh
+++ setup/install_formula.sh
@@ -1,28 +1,30 @@
 #!/bin/sh
 
+install_formula() {
+  brew update
+  
+  # essential formulas
+  brew install git
+  brew install tig
+  brew install vim
+  brew install zsh --without-etcdir
+  brew install tmux
+  brew install reattach-to-user-namespace
+  
+  # option formulas
+  brew install source-highlight
+  brew install exa
+  brew install ripgrep
+  brew install direnv
+  #brew install grv
+  #brew install the_silver_searcher
+  #brew install htop
+}
+
 # check brew is installed
-#if type "brew" > /dev/null 2>&1; then
-#    echo "brew had been already installed."
-#else
-#    echo "brew hadn't been installed."
-#    exit
-#fi
-
-brew update
-
-# essential formulas
-brew install git
-brew install tig
-brew install vim
-brew install zsh --without-etcdir
-brew install tmux
-brew install reattach-to-user-namespace
-
-# option formulas
-brew install source-highlight
-brew install exa
-brew install ripgrep
-brew install direnv
-#brew install grv
-#brew install the_silver_searcher
-#brew install htop
+if type "brew" > /dev/null 2>&1; then
+  echo "brew had been already installed."
+  install_formula
+else
+  echo "brew hadn't been installed."
+fi
