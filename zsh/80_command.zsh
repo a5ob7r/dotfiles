27c5aa2da01cb80d0b30c9f89e70da29d5c43c33
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2018-06-18 22:28:33 +0900

    Add docker fzf commands
    Interactive select images and container for delete them.

--- zsh/80_command.zsh
+++ zsh/80_command.zsh
@@ -3,3 +3,15 @@ ghq-fzf() {
   dir=$(ghq list | fzf-tmux) &&
     cd $(ghq root)/$dir
 }
+
+docker-rmi-fzf() {
+  local images
+  images=$(docker images | fzf-tmux --multi --header-lines=1) &&
+    echo $images | awk '{print $3}' | xargs docker rmi
+}
+
+docker-rm-fzf() {
+  local containers
+  containers=$(docker container ls -a | fzf-tmux --multi --header-lines=1) &&
+    echo $containers | awk '{print $1}' | xargs docker rm
+}
