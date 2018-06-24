d1f8d9ebb25775ffc118645c8b4ff1912026e08e
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2018-06-24 09:20:35 +0900

    Add Docker related commnads alias
    Often Use Docker related commnads. So want to type easily their
    commands. Especially docker-compose need so long typing.

--- zsh/30_alias.zsh
+++ zsh/30_alias.zsh
@@ -2,3 +2,11 @@ if type exa > /dev/null 2>&1; then
   alias ll='exa -lg'
   alias la='exa -lag'
 fi
+
+if type docker > /dev/null 2>&1; then
+  alias dk='docker'
+fi
+
+if type docker-compose > /dev/null 2>&1; then
+  alias dkcp='docker-compose'
+fi
