1ef08d26101b4b4d9edabb188c2471fcd8ddcb14
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2018-06-24 09:26:40 +0900

    Fix custom docker commands name
    To make uniform alias name.

--- zsh/80_command.zsh
+++ zsh/80_command.zsh
@@ -4,13 +4,13 @@ ghq-fzf() {
     cd $(ghq root)/$dir
 }
 
-docker-rmi-fzf() {
+dkrmi-fzf() {
   local images
   images=$(docker images | fzf-tmux --multi --header-lines=1) &&
     echo $images | awk '{print $3}' | xargs docker rmi
 }
 
-docker-rm-fzf() {
+dkrm-fzf() {
   local containers
   containers=$(docker container ls -a | fzf-tmux --multi --header-lines=1) &&
     echo $containers | awk '{print $1}' | xargs docker rm
