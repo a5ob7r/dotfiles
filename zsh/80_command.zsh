5cca113f04df2e7ec9fbaf0caa894f9a891029e2
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2018-06-29 02:11:01 +0900

    Fix fzf commands name
    Make easy type and set prefix `f`

--- zsh/80_command.zsh
+++ zsh/80_command.zsh
@@ -1,16 +1,16 @@
-ghq-fzf() {
+fghq() {
   local dir
   dir=$(ghq list | fzf-tmux) &&
     cd $(ghq root)/$dir
 }
 
-dkrmi-fzf() {
+fdkrmi() {
   local images
   images=$(docker images | fzf-tmux --multi --header-lines=1) &&
     echo $images | awk '{print $3}' | xargs docker rmi
 }
 
-dkrm-fzf() {
+fdkrm() {
   local containers
   containers=$(docker container ls -a | fzf-tmux --multi --header-lines=1) &&
     echo $containers | awk '{print $1}' | xargs docker rm
