0c73f9ad20541a86b95c96af37e82889f3e06ded
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2018-06-30 11:13:15 +0900

    Update install script of anyenv
    Old script is dependent on anyenv command to install anyenv-update
    plugin. But anyenv is installed by same script. Also, anyenv need load
    anyenv conf written on shell conf and this cause new dependent in the
    script. So change new script for installing anyenv and anyenv-update
    without dependent.

--- script/install_anyenv.sh
+++ script/install_anyenv.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
+set -x
+
+anyenv_root=$HOME/.anyenv
+anyenv_plugins=$anyenv_root/plugins
+anyenv_update=$anyenv_plugins/anyenv-update
+
+
 # install anyenv
 # https://github.com/riywo/anyenv
-git clone https://github.com/riywo/anyenv ~/.anyenv
+if [ ! -d $anyenv_root ]; then
+  git clone https://github.com/riywo/anyenv $anyenv_root
+fi
 
 # install anyenv update
 # https://github.com/znz/anyenv-update
-mkdir -p $(anyenv root)/plugins
-git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
+if [ ! -d $anyenv_update ]; then
+  if [ ! -d $anyenv_plugins ]; then
+    mkdir $anyenv_plugins
+  fi
+  git clone https://github.com/znz/anyenv-update.git $anyenv_update
+fi
