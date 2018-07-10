9ff62ade29dddec851b6eac4fb1273e76fb1a6dd
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2018-07-10 09:57:49 +0900

    Fix tmux powerline conf for universal environment
    Detect powerline installed with pip by calling shell script with make.

--- Makefile
+++ Makefile
@@ -8,6 +8,9 @@ link:
 unlink:
 	@$(foreach dotfile, $(DOTFILES), unlink $(HOME)/$(dotfile);)
 
+powerline-conf:
+	@etc/script/universal/link_tmux_powerline_conf.sh
+
 brew:
 	@etc/script/macos/install_brew.sh
 	@etc/script/macos/install_formula.sh
---
+++ etc/script/universal/link_tmux_powerline_conf.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+packages_root=`pip3 show powerline-status | grep "Location:" |  sed -e "s/Location: //g"`
+tmux_powerline_conf=$packages_root/powerline/bindings/tmux/powerline.conf
+
+if [[ -f $tmux_powerline_conf ]]; then
+  ln -sfv $tmux_powerline_conf $HOME/dotfiles/tmux/powerline.conf
+fi
