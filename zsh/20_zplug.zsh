963e9bc7b9420604131a1e69a95678b9d877640a
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2018-07-21 15:17:57 +0900

    Change zplug conf for fast loading

--- zsh/20_zplug.zsh
+++ zsh/20_zplug.zsh
@@ -5,7 +5,7 @@ set_zsh_plugins() {
   zplug 'zplug/zplug', hook-build:'zplug --self-manage'
   
   # plugins
-  zplug "zsh-users/zsh-completions"
+  zplug "zsh-users/zsh-completions", lazy:true
   zplug "zsh-users/zsh-autosuggestions"
   zplug "zsh-users/zsh-syntax-highlighting", defer:2
   
@@ -22,12 +22,12 @@ set_zsh_plugins() {
 
 load_zsh_plugins() {
   # Install plugins if there are plugins that have not been installed
-  if ! zplug check --verbose; then
-    printf "Install? [y/N]: "
-    if read -q; then
-      echo; zplug install
-    fi
-  fi
+  #if ! zplug check --verbose; then
+  #  printf "Install? [y/N]: "
+  #  if read -q; then
+  #    echo; zplug install
+  #  fi
+  #fi
   
   # Then, source plugins and add commands to $PATH
   zplug load
