bc52be43bd51ea2725317bd5a9103133e7658aa4
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2018-07-21 17:37:07 +0900

    Add some zsh commands
    For measurement launch zsh time and compile zsh conf files.

--- zsh/80_command.zsh
+++ zsh/80_command.zsh
@@ -15,3 +15,12 @@ fdkrm() {
   containers=$(docker container ls -a | fzf-tmux --multi --header-lines=1) &&
     echo $containers | awk '{print $1}' | xargs docker rm
 }
+
+zshtimes() {
+  for i in $(seq 1 10); do time zsh -i -c exit; done
+}
+
+zshcompiles() {
+  local f_zsh=($HOME/.zshrc $HOME/.zshenv `ls -1 $HOME/dotfiles/zsh/*.zsh`)
+  for f in $f_zsh; do zcompile $f; done
+}
