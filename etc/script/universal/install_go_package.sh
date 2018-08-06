97df4a70537e4dad11c5ab700e89fe02a68b0b9d
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2018-08-06 19:25:10 +0900

    Add package install script of python and go

---
+++ etc/script/universal/install_go_package.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+go get github.com/vim-volt/volt
+go get github.com/motemen/ghq
---
+++ etc/script/universal/install_python_package.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+pip3 install --user powerline-status
+pip3 install --user python-language-server
+pip3 install --user pipenv
