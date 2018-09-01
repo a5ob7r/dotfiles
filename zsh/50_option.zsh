55aa1e283ac182a6d5e967a5b85c06783d4fbf00
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2018-09-01 21:38:32 +0900

    Clear zsh options conf

--- zsh/50_option.zsh
+++ zsh/50_option.zsh
@@ -1,17 +1,19 @@
 setopt correct
+setopt emacs
 setopt extended_glob
 setopt extended_history
 setopt hist_expand
 setopt hist_expire_dups_first
 setopt hist_ignore_dups
 setopt hist_verify
-setopt ignoreeof
+setopt ignore_eof
 setopt list_packed
 setopt magic_equal_subst
 setopt mark_dirs
-setopt no_flow_control
-setopt nobeep
 setopt print_eight_bit
 setopt prompt_subst
 setopt pushd_ignore_dups
 setopt share_history
+
+unsetopt beep
+unsetopt flow_control
--- zsh/60_other.zsh
+++ zsh/60_other.zsh
@@ -1,5 +1,3 @@
-bindkey -e
-
 # for direnv
 if (( $+commands[direnv] )); then
   eval "$(direnv hook zsh)"
