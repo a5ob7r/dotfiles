de8eba1afb5c886a2542fadb20bffe897b5d2264
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2018-09-03 15:07:28 +0900

    Add auto install zplugin and fzf to installer

--- etc/install.sh
+++ etc/install.sh
@@ -4,3 +4,8 @@ DOTFILES="$HOME/src/github.com/a5ob7r/dotfiles"
 DOTFILES_REMOTE='https://github.com/a5ob7r/dotfiles.git'
 
 git clone $DOTFILES_REMOTE $DOTFILES
+
+cd $DOTFILES
+
+./etc/script/universal/install_zplugin.sh
+./etc/script/universal/install_fzf.sh
