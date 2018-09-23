6dafcc6022583e8534cba82ee54968c7ad9732c8
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2018-09-23 12:38:15 +0900

    Fix install.sh with shellcheck

--- etc/install.sh
+++ etc/install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-DOTFILES="$HOME/src/github.com/a5ob7r/dotfiles"
+DOTFILES=~/src/github.com/a5ob7r/dotfiles
 DOTFILES_REMOTE='https://github.com/a5ob7r/dotfiles.git'
 
-git clone $DOTFILES_REMOTE $DOTFILES
+git clone "$DOTFILES_REMOTE" "$DOTFILES"
 
-cd $DOTFILES
+cd "$DOTFILES" || exit
 
 ./etc/script/universal/install_zplugin.sh
 ./etc/script/universal/install_fzf.sh
