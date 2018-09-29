021cb366f95dda9880056a9991b2d89f2557e34d
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2018-09-29 10:18:36 +0900

    Fix brew formula install script
    Separate formulas list from script.

--- etc/script/macos/install_formula.sh
+++ etc/script/macos/install_formula.sh
@@ -1,35 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 install_formula() {
   brew update
 
-  local formulas=( \
-    "git" \
-    "tig" \
-    "vim" \
-    "zsh --without-etcdir" \
-    "tmux" \
-    "exa" \
-    "ripgrep" \
-    "direnv" \
-    "python" \
-    "hub" \
-    "coreutils" \
-    "bash" \
-    "ctags" \
-    "go" \
-    "htop" \
-    "gibo" \
-    )
-  
-  for ((i=0; i<${#formulas[@]}; i++)); do
-    brew install ${formulas[i]}
-  done
+  while read -r formula; do
+    brew install "$formula"
+  done < ./formulas
 }
 
 # check brew is installed
 if type "brew" > /dev/null 2>&1; then
   echo "brew had been already installed."
+  cd "$(dirname "$0")" || exit
   install_formula
 else
   echo "brew hadn't been installed."
