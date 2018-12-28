c9a28a5acf482f6d09d183322994d371cbe425cf
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2018-12-28 22:24:58 +0900

    Add check git command for installer

--- etc/install.sh
+++ etc/install.sh
@@ -3,6 +3,12 @@
 DOTFILES=~/src/github.com/a5ob7r/dotfiles
 DOTFILES_REMOTE='https://github.com/a5ob7r/dotfiles.git'
 
+if ! type git > /dev/null 2>&1; then
+  echo 'Not found git.'
+  echo 'Please install git.'
+  exit 1
+fi
+
 git clone "$DOTFILES_REMOTE" "$DOTFILES"
 
 cd "$DOTFILES" || exit
