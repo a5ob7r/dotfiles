7f6a30e201bc85a4332a215fe1e8f3127b06a0f8
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2019-06-27 18:10:12 +0900

    Remove `function` from shell function definition
    No need `function` expression when define shell functions.

--- zsh/80_command.zsh
+++ zsh/80_command.zsh
@@ -1,31 +1,31 @@
 # execute whenever the current working directory is changed
-function chpwd() {
+chpwd() {
   ll
 }
 
-function fghq() {
+fghq() {
   local dir
   dir=$(ghq list | fzf-tmux) &&
     cd $(ghq root)/$dir
 }
 
-function fdkrmi() {
+fdkrmi() {
   local images
   images=$(docker images | fzf-tmux --multi --header-lines=1) &&
     echo $images | awk '{print $3}' | xargs docker rmi
 }
 
-function fdkrm() {
+fdkrm() {
   local containers
   containers=$(docker container ls -a | fzf-tmux --multi --header-lines=1) &&
     echo $containers | awk '{print $1}' | xargs docker rm
 }
 
-function zshtimes() {
+zshtimes() {
   for i in $(seq 1 10); do time zsh -i -c exit; done
 }
 
-function zshcompiles() {
+zshcompiles() {
   local f_zsh=(~/.zshrc ~/.zshenv `ls -1 $DOTFILES/zsh/*.zsh`)
   for f in $f_zsh; do zcompile $f; done
 }
