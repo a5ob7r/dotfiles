34a37527b735d41e3ae145ceb46f7e9795492db7
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2019-07-14 11:33:31 +0900

    Update shell function used fzf

--- zsh/70_fzf.zsh
+++ zsh/70_fzf.zsh
@@ -1,25 +1,8 @@
 # Auto-completion
-[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
+source ~/.fzf/shell/completion.zsh 2> /dev/null
 
 # Key bindings
 # ------------
-if [[ $- == *i* ]]; then
-
-# CTRL-T - Paste the selected file path(s) into the command line
-__fsel() {
-  local cmd="${FZF_CTRL_T_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
-    -o -type f -print \
-    -o -type d -print \
-    -o -type l -print 2> /dev/null | cut -b3-"}"
-  setopt localoptions pipefail 2> /dev/null
-  eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS" $(__fzfcmd) -m "$@" | while read item; do
-    echo -n "${(q)item} "
-  done
-  local ret=$?
-  echo
-  return $ret
-}
-
 __fzf_use_tmux__() {
   [ -n "$TMUX_PANE" ] && [ "${FZF_TMUX:-0}" != 0 ] && [ ${LINES:-40} -gt 15 ]
 }
@@ -29,46 +12,31 @@ __fzfcmd() {
     echo "fzf-tmux -d${FZF_TMUX_HEIGHT:-40%}" || echo "fzf"
 }
 
-#fzf-file-widget() {
-#  LBUFFER="${LBUFFER}$(__fsel)"
-#  local ret=$?
-#  zle redisplay
-#  typeset -f zle-line-init >/dev/null && zle zle-line-init
-#  return $ret
-#}
-#zle     -N   fzf-file-widget
-#bindkey '^T' fzf-file-widget
+__fsel() {
+  local cmd="find . -mindepth 1 -maxdepth 1 -print | cut -b 3-"
+  local FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS"
+  setopt localoptions pipefail 2> /dev/null
 
-# Ensure precmds are run after cd
-#fzf-redraw-prompt() {
-#  local precmd
-#  for precmd in $precmd_functions; do
-#    $precmd
-#  done
-#  zle reset-prompt
-#}
-#zle -N fzf-redraw-prompt
+  eval "$cmd" |
+    $(__fzfcmd) --multi |
+    while read -r item; do echo -n "${(q)item} "; done
 
-# ALT-C - cd into the selected directory
-#fzf-cd-widget() {
-#  local cmd="${FZF_ALT_C_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
-#    -o -type d -print 2> /dev/null | cut -b3-"}"
-#  setopt localoptions pipefail 2> /dev/null
-#  local dir="$(eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS" $(__fzfcmd) +m)"
-#  if [[ -z "$dir" ]]; then
-#    zle redisplay
-#    return 0
-#  fi
-#  cd "$dir"
-#  local ret=$?
-#  zle fzf-redraw-prompt
-#  typeset -f zle-line-init >/dev/null && zle zle-line-init
-#  return $ret
-#}
-#zle     -N    fzf-cd-widget
-#bindkey '\ec' fzf-cd-widget
+  local ret=$?
+  echo
 
-# CTRL-R - Paste the selected command from history into the command line
+  return $ret
+}
+
+fzf-file-widget() {
+  LBUFFER="${LBUFFER}$(__fsel)"
+  local ret=$?
+  zle redisplay
+  typeset -f zle-line-init >/dev/null && zle zle-line-init
+  return $ret
+}
+zle     -N   fzf-file-widget
+bindkey '^T' fzf-file-widget
+
 fzf-history-widget() {
   local selected num
   setopt localoptions noglobsubst noposixbuiltins pipefail 2> /dev/null
@@ -88,4 +56,3 @@ fzf-history-widget() {
 zle     -N   fzf-history-widget
 bindkey '^R' fzf-history-widget
 
-fi
