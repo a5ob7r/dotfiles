bda9a319d96002a939a7087fef4e821be12b2b42
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2019-07-27 23:19:17 +0900

    Fix tmux configures by using shell script
    This way make complicated configures more easy.

---
+++ .tmux/tmux.20_opt.sh
@@ -0,0 +1,29 @@
+source tmux.lib.sh
+
+# {{{ default shell
+tmux set -g default-command "$SHELL"
+# }}}
+
+# {{{ terminal type
+# use true color in tmux
+tmux set -g default-terminal "xterm-256color"
+tmux set -ga terminal-overrides ",xterm-256color:Tc"
+# }}}
+
+# {{{ operating style
+tmux set -g mouse on
+tmux setw -g mode-keys vi
+tmux set -g status-keys emacs
+tmux set -sg escape-time 0
+# }}}
+
+# {{{ history
+tmux set -g history-file "$HOME/.tmux_history"
+tmux set -g history-limit 10000
+# }}}
+
+# {{{ command alias
+if is_tmux_version "> 2.4"; then
+  tmux set -sg command-alias[0] e="split-window -c '#{pane_current_path}'"
+fi
+# }}}
