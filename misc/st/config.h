a9b0208d5eaf176691d88cc322d132d940f4fd62
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2024-04-14 16:47:39 +0900

    Follow some st configurations to the upstream

--- misc/st/config.h
+++ misc/st/config.h
@@ -62,7 +62,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Monospace:pixelsize=32:antialias=true:autohint=false";
-static int borderpx = 10;
+static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -109,7 +109,7 @@ int allowwindowops = 0;
  * near minlatency, but it waits longer for slow updates to avoid partial draw.
  * low minlatency will tear/flicker more, as it can "detect" idle too early.
  */
-static double minlatency = 8;
+static double minlatency = 2;
 static double maxlatency = 33;
 
 /*
